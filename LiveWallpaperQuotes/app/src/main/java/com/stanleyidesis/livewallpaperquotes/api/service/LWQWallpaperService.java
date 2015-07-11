35af4eafe06fc8ea5cbe34358eda91ebf30eec8e
Stan Idesis <stan.idesis@gmail.com>
2015-07-11 16:16:51 -0700

    Likely everything required to recognize this application as a live wallpaper

---
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/livewallpaperquotes/api/service/LWQWallpaperService.java
@@ -0,0 +1,90 @@
+package com.stanleyidesis.livewallpaperquotes.api.service;
+
+import android.os.Bundle;
+import android.service.wallpaper.WallpaperService;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.WindowInsets;
+
+/**
+ * Created by stanleyidesis on 7/11/15.
+ */
+public class LWQWallpaperService extends WallpaperService {
+
+    public class LWQWallpaperEngine extends Engine {
+
+        @Override
+        public void onApplyWindowInsets(WindowInsets insets) {
+            super.onApplyWindowInsets(insets);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public Bundle onCommand(String action, int x, int y, int z, Bundle extras, boolean resultRequested) {
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+            return super.onCommand(action, x, y, z, extras, resultRequested);
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            super.onCreate(surfaceHolder);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onDesiredSizeChanged(int desiredWidth, int desiredHeight) {
+            super.onDesiredSizeChanged(desiredWidth, desiredHeight);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onOffsetsChanged(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep, int xPixelOffset, int yPixelOffset) {
+            super.onOffsetsChanged(xOffset, yOffset, xOffsetStep, yOffsetStep, xPixelOffset, yPixelOffset);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            super.onSurfaceChanged(holder, format, width, height);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onSurfaceCreated(SurfaceHolder holder) {
+            super.onSurfaceCreated(holder);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onSurfaceRedrawNeeded(SurfaceHolder holder) {
+            super.onSurfaceRedrawNeeded(holder);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onTouchEvent(MotionEvent event) {
+            super.onTouchEvent(event);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            Log.v(getClass().getSimpleName(), null, new Throwable());
+        }
+
+    }
+
+    @Override
+    public Engine onCreateEngine() {
+        return new LWQWallpaperEngine();
+    }
+
+}
---
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/livewallpaperquotes/ui/activity/LWQSettingsActivity.java
@@ -0,0 +1,9 @@
+package com.stanleyidesis.livewallpaperquotes.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by stanleyidesis on 7/11/15.
+ */
+public class LWQSettingsActivity extends Activity {
+}
