e6bee50d5cf159775efcfb725951c0c488445f86
Stan Idesis <stan.idesis@gmail.com>
2015-07-11 15:39:45 -0700

    Initial project commit

---
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/livewallpaperquotes/api/service/LWPWallpaperService.java
@@ -0,0 +1,15 @@
+package com.stanleyidesis.livewallpaperquotes.api.service;
+
+import android.service.wallpaper.WallpaperService;
+
+/**
+ * Created by stanleyidesis on 7/11/15.
+ */
+public class LWPWallpaperService extends WallpaperService {
+
+    @Override
+    public Engine onCreateEngine() {
+        return null;
+    }
+    
+}
