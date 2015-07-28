f0dd2afe1d7d1346af3b24b4784391963c014745
Stan Idesis <stan.idesis@gmail.com>
2015-07-27 23:22:10 -0700

    [#3] A lot of struggling, there's either a bug with the flickr API or my query params are busted

--- LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/livewallpaperquotes/ui/activity/LWQSettingsActivity.java
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/livewallpaperquotes/ui/activity/LWQSettingsActivity.java
@@ -1,9 +1,17 @@
 package com.stanleyidesis.livewallpaperquotes.ui.activity;
 
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.googlecode.flickrjandroid.photos.Photo;
+import com.googlecode.flickrjandroid.photos.PhotoList;
+import com.stanleyidesis.livewallpaperquotes.R;
 import com.stanleyidesis.livewallpaperquotes.api.db.Quote;
+import com.stanleyidesis.livewallpaperquotes.api.network.Callback;
+import com.stanleyidesis.livewallpaperquotes.api.network.FlickrManager;
 
 import java.util.Iterator;
 
@@ -11,6 +19,34 @@ import java.util.Iterator;
  * Created by stanleyidesis on 7/11/15.
  */
 public class LWQSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lwq_settings);
+        FlickrManager.getInstance(this).searchFlickr(new String[]{"landmark", "scenic"}, new Callback<PhotoList>() {
+            @Override
+            public void onSuccess(final PhotoList returnValue) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < returnValue.size(); i++) {
+                            final Photo photo = returnValue.get(i);
+                            Log.v(getClass().getSimpleName(), photo.getLarge2048Url());
+                        }
+                        Uri uri = Uri.parse(returnValue.get(0).getLarge2048Url());
+                        SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
+                        draweeView.setImageURI(uri);
+                    }
+                });
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+
+            }
+        });
+    }
 
     @Override
     protected void onResume() {
