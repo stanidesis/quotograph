07c7f2e98dcfb16d7fb58b59730a257a7042aea0
Stanley Idesis <stan.idesis@gmail.com>
2016-01-10 14:09:42 -0800

    [#87] Integrated Crashlytics

--- LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/quotograph/LWQApplication.java
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/quotograph/LWQApplication.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.orm.SugarApp;
 import com.orm.query.Condition;
@@ -25,6 +26,7 @@ import com.stanleyidesis.quotograph.api.db.Quote;
 import com.stanleyidesis.quotograph.api.network.NetworkConnectionListener;
 import com.stanleyidesis.quotograph.api.receiver.LWQReceiver;
 import com.stanleyidesis.quotograph.api.service.LWQWallpaperService;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Copyright (c) 2015 Stanley Idesis
@@ -72,6 +74,7 @@ public class LWQApplication extends SugarApp {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Fresco.initialize(this);
 
         sApplication = this;
