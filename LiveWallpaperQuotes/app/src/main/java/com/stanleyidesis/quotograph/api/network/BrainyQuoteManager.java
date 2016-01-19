65ebbffc35d59bb68a313ad5463e4c560c1f6698
Stanley Idesis <stan.idesis@gmail.com>
2016-01-18 19:06:29 -0800

    [#98] Improved Crashlytics 7 fix

--- LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/quotograph/api/network/BrainyQuoteManager.java
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/quotograph/api/network/BrainyQuoteManager.java
@@ -216,15 +216,15 @@ public class BrainyQuoteManager {
             if (authors == null || authors.isEmpty()) {
                 continue;
             }
-            BrainyQuote brainyQuote = new BrainyQuote();
-            brainyQuote.quote = quotes.get(0).text().replace("\"", "");
-            brainyQuote.author = new BrainyAuthor(authors.get(0).text());
-            brainyQuotes.add(brainyQuote);
             final Elements category = quoteAuthorCategories.select("a[href^=/quotes/topics]");
             if (category == null || category.isEmpty() || category.get(0).text() == null) {
                 continue;
             }
+            BrainyQuote brainyQuote = new BrainyQuote();
+            brainyQuote.quote = quotes.get(0).text().replace("\"", "");
+            brainyQuote.author = new BrainyAuthor(authors.get(0).text());
             brainyQuote.topic = new BrainyTopic(category.get(0).text());
+            brainyQuotes.add(brainyQuote);
         }
         return new ArrayList<>(new LinkedHashSet<>(brainyQuotes));
     }
