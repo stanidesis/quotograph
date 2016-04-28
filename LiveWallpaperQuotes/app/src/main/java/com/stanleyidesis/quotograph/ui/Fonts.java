7b0da5d78a85e3ba98d992aa9591fbaa1d00de18
Stanley Idesis <stan.idesis@gmail.com>
2016-04-27 22:18:54 -0700

    [#141] Improved the font selection

--- LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/quotograph/ui/Fonts.java
+++ LiveWallpaperQuotes/app/src/main/java/com/stanleyidesis/quotograph/ui/Fonts.java
@@ -13,16 +13,18 @@ public enum Fonts {
 
     // DEFAULTS @ 0
     SYSTEM(0, R.string.font_system, null),
+    ABRIL_FATFACE(11, R.string.font_abril_fatface, "AbrilFatface-Regular.ttf"),
     AMATIC_SC_BOLD(1, R.string.font_amatic_sc, "AmaticSC-Bold.ttf"),
+    ANTON(12, R.string.font_anton, "Anton.ttf"),
     BLACK_OPS_ONE_REGULAR(2, R.string.font_black_ops_one, "BlackOpsOne-Regular.ttf"),
-    CRAFTY_GIRLS(3, R.string.font_crafty_girls, "CraftyGirls.ttf"),
+    CABIN_SKETCH(13, R.string.font_cabin_sketch, "CabinSketch-Bold.ttf"),
     FINGER_PAINT_REGULAR(4, R.string.font_finger_paint, "FingerPaint-Regular.ttf"),
-    INDIE_FLOWER(5, R.string.font_indie_flower, "IndieFlower.ttf"),
+    FREDERICKA_THE_GREAT(14, R.string.font_fredericka_the_great, "FrederickatheGreat-Regular.ttf"),
     JOSEFIN_BOLD(6, R.string.font_josefin_sans_bold, "JosefinSans-Bold.ttf"),
-    PACIFICO(7, R.string.font_pacifico, "Pacifico.ttf"),
-    PRESS_START_2P(8, R.string.font_press_start_2p, "PressStart2P-Regular.ttf"),
     SPECIAL_ELITE(9, R.string.font_special_elite, "SpecialElite.ttf"),
-    UNICA_ONE(10, R.string.font_unica_one, "UnicaOne-Regular.ttf");
+    SPIRAX(15, R.string.font_special_elite, "Spirax-Regular.ttf"),
+    UNICA_ONE(10, R.string.font_unica_one, "UnicaOne-Regular.ttf"),
+    VT323(16, R.string.font_vt323, "VT323-Regular.ttf");
 
     int id;
     int prettyName;
